3251c77d1e676494b8b07d4281f900d6d241f5fd
julianeschrittenlacher <julianeschrittenlacher@gmail.com>
2024-06-03 11:54:52 +0200

    add Player, Controller and Interface Playable

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MusicPlayer player1 = new MusicPlayer();
+        VideoPlayer player2 = new VideoPlayer();
+        MediaController mediaController = new MediaController();
+
+        mediaController.playMedia(player1);
+        mediaController.playMedia(player2);
+
     }
 }
